6704c52a31480fb4014bb4e542a25f27488a60c7
Davor Tvoric <davor.tvoric@gmail.com>
2020-03-28 16:23:36 +0100

    feat(config) Add prettier, eslint, npm scripts, parcel and separate to modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement(
-        "div",
-        {},
-        [React.createElement("h1", {}, name)],
-        [React.createElement("h2", {}, animal)],
-        [React.createElement("h2", {}, breed)],
-    )
-}
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me"),
-            React.createElement(Pet, {name: "Čupko", animal: "Cat", breed: "Maine coon"}),
-            React.createElement(Pet, {name: "Pablo", animal: "Cat", breed: "Mixed"}),
-            React.createElement(Pet, {name: "Oskar", animal: "Dog", breed: "Maltese"}),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Čupko",
+      animal: "Cat",
+      breed: "Maine coon",
+    }),
+    React.createElement(Pet, { name: "Pablo", animal: "Cat", breed: "Mixed" }),
+    React.createElement(Pet, {
+      name: "Oskar",
+      animal: "Dog",
+      breed: "Maltese",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
