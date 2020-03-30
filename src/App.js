e1127ddf26319f3ead3cc75b4fdbecc5c753dc40
Davor Tvoric <davor.tvoric@gmail.com>
2020-03-30 17:52:22 +0200

    feat(filtering) Create hooks for filtering the data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="Čupko" animal="Cat" breed="Maine coon" />
-      <Pet name="Pablo" animal="Cat" breed="Mixed" />
-      <Pet name="Oskar" animal="Dog" breed="Maltese" />
+      <SearchParams />
     </div>
   );
 };
