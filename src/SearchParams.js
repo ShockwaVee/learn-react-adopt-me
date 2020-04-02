6b02e10ecae7735ebf29b5d95b88ee870e8af958
Davor Tvoric <davor.tvoric@gmail.com>
2020-04-02 19:47:01 +0200

    feat(apiData) Async calls to Petfinder API, update Pet component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results.js";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
 
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+    console.log(animals);
+    setPets(animals || []);
+  }
   useEffect(() => {
     setBreeds([]);
     setBreed("");
@@ -20,7 +31,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -30,11 +46,12 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
             placeholder="Location"
           />
-          <button>Submit</button>
         </label>
         <AnimalDropdown />
         <BreedDropdown />
+        <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
